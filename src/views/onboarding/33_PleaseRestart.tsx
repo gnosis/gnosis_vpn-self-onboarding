ff06e7c97384bb4e70582544d6fe44bf706a17a3
mjadach-iv <michal.jadach@hoprnet.org>
2026-02-16 19:31:59 +0200

    remove unused imports

--- src/views/onboarding/33_PleaseRestart.tsx
+++ src/views/onboarding/33_PleaseRestart.tsx
@@ -1,4 +1,3 @@
-import { Typography } from "@mui/material";
 import Button from "../../components/onboarding/Button";
 import Step from "../../components/onboarding/Step";
 import { useAppStore } from "../../store/appStore";
