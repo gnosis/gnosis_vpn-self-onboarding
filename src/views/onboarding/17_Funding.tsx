6df1cc77746a3a281095cb1d20c1a25a9d2547f9
Michał Jadach <michal.jadach@hoprnet.org>
2026-03-06 13:00:33 +0100

    Adjust bold words

--- src/views/onboarding/17_Funding.tsx
+++ src/views/onboarding/17_Funding.tsx
@@ -226,7 +226,7 @@ export default function Funding({ className, lastEntry }: FundingProps) {
                   color: "#333",
                 }}
               >
-                After funding, the node needs a few minutes to sync. This is normal, nothing is broken. Please note the time when syncing starts and let me know how long it took.
+                <strong>After funding, the node needs a few minutes to sync. This is normal, nothing is broken. Please note the time when syncing starts and let me know how long it took.</strong>
               </Typography>
 
           </Typography>
