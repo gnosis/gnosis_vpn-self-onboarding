aa4a46be648de807c462522198f52f12188fc65e
Andrius <73285987+Jaguaras@users.noreply.github.com>
2026-04-22 15:07:41 +0300

    Changing button purpose
    I want to change from "I am already connected to VPN" to "I need some help"

--- src/views/onboarding/22_ReadyToTest.tsx
+++ src/views/onboarding/22_ReadyToTest.tsx
@@ -15,7 +15,7 @@ export default function ReadyToTest({ className, lastEntry }: ReadyToTestProps) 
   const saveAnswer = useAppStore((state) => state.saveAnswer);
   const exitNodeIteration = useAppStore((state) => state.exitNodeIteration);
 
-  const ALREADY_DID_LABEL = "I already connected to the VPN";
+  const NEED_HELP_LABEL = "I need some help";
   const OKAY_LABEL = "It's synced";
 
   const handleAnswer = (answer: string, nextStep: number) => {
@@ -57,7 +57,7 @@ export default function ReadyToTest({ className, lastEntry }: ReadyToTestProps) 
         lastEntry
         ? (
           <>
-            <Button label={ALREADY_DID_LABEL} onClick={() => handleAnswer(ALREADY_DID_LABEL, STEP + 1)} />
+            <Button label={ALREADY_DID_LABEL} onClick={() => handleAnswer(NEED_HELP_LABEL, STEP + 0.25)} />
             <Button label={OKAY_LABEL} onClick={() => handleAnswer(OKAY_LABEL, STEP + 3)} />
           </>
         ) : null
